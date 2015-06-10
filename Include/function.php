a8b4b68fc70fefb0d4c1e54ccd7e1191a5c15ead
acabrera00 <acabrera1900@hotmail.com>
2015-06-10 12:06:17 -0400

    Create function.php

---
+++ Include/function.php
@@ -0,0 +1,24 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+function grabFName()
+{
+	$tempVar = "First";
+        $gfName = $tempVar;
+        return $gfName;
+}
+
+function grabLName()
+{
+	$tempVar = "First";
+        $gfName = $tempVar;
+        return $glName;
+}
+
+function grabEMail()
+{
+	$tempVar = "mail@mailer.mail";
+        $gfName = $tempVar;
+        return $geMail;
+}
+?>
