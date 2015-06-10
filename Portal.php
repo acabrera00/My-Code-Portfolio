e2833cbe7c5a18a8c1d6206df1c8185cc360b035
acabrera00 <acabrera1900@hotmail.com>
2015-06-10 12:02:59 -0400

    Create Portal.php

---
+++ Portal.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+            <style type="text/css">
+                    <!--
+                    .style1 {
+                            font-size: 12px;
+                            font-weight: bold;
+                    }
+                    -->
+            </style>
+        <title>Portal</title>
+    </head>
+    
+    <body>
+        <div id="pageContainer">
+            <div id="mainContentAdmin">
+                <div style="width: 830px;text-align: center;">
+                    <div class="contentdiv">
+                        <div style="display: table; margin: 0 auto;" class="contentdiv">
+                            <table style="text-align:center;" width="465" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                        <td align="center" valign="middle" class="cpanelTop">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td class="cpanelRow">
+					<table width="455" height="85"border="0" cellspacing="0" cellpadding="2" align="center">
+                                            <tr>
+                                                <td width="110" align="center" valign="Top">
+                                                    <a href="Progress_Report.php"><img src="image/test.png"  width="80" height="62" border="0" /><br />Progress Report</a>
+                                                </td>
+
+                                                <td width="110" align="center" valign="top">
+                                                    <a href="Authorize.php"><img src="image/test.png"  width="80" height="62" border="0" /><br />New User Authorization</a>
+                                                </td>
+                                                
+                                                <td width="110" align="center" valign="Top">
+                                                    <a href="Test_Report.php"><img src="image/tes.png"  width="80" height="62" border="0" /><br />Testing Report</a>
+                                                </td>
+
+                                                <td width="110"  align="center" valign="top">
+                                                    <a href="ChangePass.php"><img src="image/test.png"  width="80" height="62" border="0" /><br />Change Password</a>
+                                                </td>
+                                            </tr>
+					</table>
+                                    </td>
+                                </tr>
+                            </table>
+                            <br>
+                            <center><p><a href="Portal.php">Return to Portal</a></p></center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
