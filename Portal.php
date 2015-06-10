d26fd8059bf6063e26ea4add924fad5de602c0eb
acabrera00 <acabrera1900@hotmail.com>
2015-06-10 12:07:53 -0400

    Update Portal.php

--- Portal.php
+++ Portal.php
@@ -1,3 +1,7 @@
+<?php
+    include_once 'include/function.php';
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
